58e0a3fe691901c61bdc8e9fc9007d48ef583a8e
Maksat <mkartanbaev93@gmail.com>
2023-02-08 12:39:00 +0600

    loops

--- basics/test.py
+++ basics/test.py
@@ -1,7 +1,4 @@
-# создайте список от 1 до 10
-# выведите список с числами в обратном порядке
-list1 = [1,2,3,4,5,6,7,8,9,10]
-list1 = list(range(1,11))
-print(list1[::-1])
-list1.reverse()
-print(list(reversed(list1)))
+list1 = [1,2,3,1,2,3,1,1,1,2,3,1,1]
+while 1 in list1:
+    list1.remove(1)
+print(list1)
